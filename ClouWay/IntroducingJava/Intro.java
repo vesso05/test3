8eb5308bec4227f309a6409ca4b3fb727c2d2492
Vesso05 <Vesso05@mail.com>
2019-04-23 14:42:15 +0300

    Finds the greatest common divisor of two numbers

--- ClouWay/IntroducingJava/Intro.java
+++ ClouWay/IntroducingJava/Intro.java
@@ -5,5 +5,28 @@ import java.util.stream.IntStream;
 public class Intro {
 
 
+    /**
+     * Finds the greatest common divisor of two numbers.
+     * @param a integer entered by the user
+     * @param b integer entered by the user
+     * @return the greatest common divisor of both
+     */
+  public static int gcd(int a, int b)
+  {
+      while (a != b)
+      {
+          if (a > b)
+          {
+              a -= b;
+          }
+           else
+          {
+              b -= a;
+          }
+      }
+      return a;
+
+  }
+
 
 }
